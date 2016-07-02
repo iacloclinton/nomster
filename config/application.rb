81a087598d0f05904c5a8ae2363871dc042bef9c
ighodaro clinton <ighodaroclinton@gmail.com>
2016-07-02 15:20:59 +0200

    finished chapter 11

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,10 @@ module Nomster
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+       # Add the fonts path
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+
+    # Precompile additional assets
+    config.assets.precompile += %w( .svg .eot .woff .ttf )
   end
 end
