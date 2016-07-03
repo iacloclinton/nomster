973818017a0534b30a39067263bebe95ad7c1aea
ighodaro clinton <ighodaroclinton@gmail.com>
2016-07-03 23:30:10 +0200

    finished chapter 14

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -6,13 +6,16 @@ class PlacesController < ApplicationController
   def new
     @place = Place.new
   end
+
   def create
-  	place.create(place_params)
-  	redirect_to root_path
-  end
-  private
-  def place_params
-  	params.request(:place).permit(:name, :description, :address)
+    Place.create(place_params)
+    redirect_to root_path
   end
 
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+  end
 end
+
